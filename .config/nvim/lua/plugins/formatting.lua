60b95a73e0ce7181292f96386fb5833890efb6af
klaus-224 <rohinesh.ram@protonmail.com>
2025-04-03 16:40:00 -0600

    replace formatting with conform

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,36 +1,42 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            "<leader>gf",
-            function()
-                require("conform").format({ async = true, lsp_fallback = true })
-            end,
-            desc = "Format buffer",
-        },
-    },
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                markdown = { "prettier" },
-            },
-            -- Set up format-on-save
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_fallback = true,
-            },
-        })
-    end,
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>gf",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			desc = "Format buffer",
+		},
+	},
+	config = function()
+		require("conform").setup({
+			formatters = {
+				prettier = {
+					options = {
+						single_quote = true,
+					},
+				}
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier", stop_after_first = true },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier", },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+			},
+			-- Set up format-on-save
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+	end,
 }
-
