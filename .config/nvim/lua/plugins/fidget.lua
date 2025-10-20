cf0f152d5282611506e905bee00e3773d6a9ee9c
klaus-224 <rohinesh.ram@protonmail.com>
2025-10-20 17:11:38 -0600

    simple

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -2,7 +2,7 @@ return {
 	"j-hui/fidget.nvim",
 	opts = {
 		notification = {
-			override_vim_notify = true, -- Automatically override vim.notify() with Fidget
+			override_vim_notify = true, -- override vim.notify()
 		}
 	},
 }
