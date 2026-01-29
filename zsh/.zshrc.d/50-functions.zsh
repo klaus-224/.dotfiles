9649c77408b0bfdaae15d56d326ca77807f1de72
klaus-224 <rohinesh.ram@protonmail.com>
2026-01-29 09:30:44 -0700

    updates for fzf

--- zsh/.zshrc.d/50-functions.zsh
+++ zsh/.zshrc.d/50-functions.zsh
@@ -14,18 +14,3 @@
 #   - No tool initialization
 # --------------------------------------------------
 
-# Git branch switcher with fzf
-fbr() {
-  local branches branch
-  branches=$(git branch -a) &&
-  branch=$(echo "$branches" | fzf +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
-
-# Zle widget for keybinding
-fzf-git-branch-widget() {
-  fbr
-  zle reset-prompt
-}
-zle -N fzf-git-branch-widget
-bindkey '^b' fzf-git-branch-widget  # Ctrl+B
