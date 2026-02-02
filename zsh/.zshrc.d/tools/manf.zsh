84d7162e173ec1ad4d1b79e73626c29efffcbfca
R-Ram95 <rohinesh@ualberta.ca>
2026-02-02 07:42:17 -0700

    WIP

--- zsh/.zshrc.d/tools/manf.zsh
+++
@@ -1,8 +0,0 @@
-function manf() {
-  man -k . \
-  | fzf --prompt='man> ' \
-        --preview 'man {1} | col -bx | bat -l man -p' \
-        --preview-window=right:70% \
-  | awk '{print $1}' \
-  | xargs man
-}
