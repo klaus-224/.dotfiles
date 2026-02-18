b06a68f54123288a892b303c50b64aad0305e5d7
R-Ram95 <rohinesh@ualberta.ca>
2026-02-17 22:49:39 -0700

    more stuff

--- nvim/.config/nvim/lua/plugins/color-scheme.lua
+++ nvim/.config/nvim/lua/plugins/color-scheme.lua
@@ -10,21 +10,21 @@ return {
 			vim.cmd.colorscheme("vague")
 		end,
 	},
-	{
-		"gorgoroth",
-		name = "gorgoroth",
-		lazy = true,
-		priority = 1000,
-		config = function()
-			require("black-metal").setup({
-				theme = "gorgoroth",
-				code_style = {
-					comments = "none",
-					headings = "bold", -- Markdown headings
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"gorgoroth",
+	-- 	name = "gorgoroth",
+	-- 	lazy = true,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("black-metal").setup({
+	-- 			theme = "gorgoroth",
+	-- 			code_style = {
+	-- 				comments = "none",
+	-- 				headings = "bold", -- Markdown headings
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
