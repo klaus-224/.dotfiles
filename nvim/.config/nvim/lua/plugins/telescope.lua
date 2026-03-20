1a37d30f3dd63fd2c1b00a09c7dda68e277b2cf0
R-Ram95 <rohinesh@ualberta.ca>
2026-03-20 07:16:30 -0600

    stuff

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -127,17 +127,17 @@ return {
 			require("custom.multigrep").setup()
 		end,
 	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+	-- {
+	-- 	"nvim-telescope/telescope-ui-select.nvim",
+	-- 	config = function()
+	-- 		require("telescope").setup({
+	-- 			extensions = {
+	-- 				["ui-select"] = {
+	-- 					require("telescope.themes").get_dropdown({}),
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 		require("telescope").load_extension("ui-select")
+	-- 	end,
+	-- },
 }
