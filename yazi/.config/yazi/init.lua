98f86d04c8639c4abd5465d06fae3b074acd93d5
klaus <klaus224@klauss-MacBook-Pro.local>
2026-04-05 10:00:12 -0600

    remove yazi

--- yazi/.config/yazi/init.lua
+++
@@ -1,6 +0,0 @@
--- DuckDB plugin configuration
-require("duckdb"):setup({
-	-- mode = "standard" / "summarized", -- Default: "summarized"
-	-- cache_size = 1000,                -- Default: 500
-	-- row_id = true / false / "dynamic", -- Default: false
-})
