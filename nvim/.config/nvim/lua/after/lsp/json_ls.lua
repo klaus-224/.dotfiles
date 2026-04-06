9f4175df3396f7ac99929f957e0d433c8ee39409
klaus <klaus224@klauss-MacBook-Pro.local>
2026-04-05 19:33:47 -0600

    final touches

--- nvim/.config/nvim/lua/after/lsp/json_ls.lua
+++ nvim/.config/nvim/lua/after/lsp/json_ls.lua
@@ -3,6 +3,12 @@ local client_config = require("lsp.client-config")
 return vim.tbl_deep_extend("force", client_config.base(), {
 	cmd = { "vscode-json-language-server", "--stdio" },
 	filetypes = { "json", "jsonc" },
+	settings = {
+		json = {
+			schemas = require('schemastore').json.schemas(),
+			validate = { enable = true },
+		},
+	},
 	init_options = {
 		provideFormatter = true,
 	},
