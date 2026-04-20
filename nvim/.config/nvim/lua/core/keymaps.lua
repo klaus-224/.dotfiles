d4871c3fbb4f04ae8859c33bde46140c5aa9fb94
klaus-224 <rohinesh.ram@protonmail.com>
2026-04-20 12:20:09 -0600

    nvim/lsp

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,7 +1,5 @@
 local set = vim.keymap.set
 local k = vim.keycode
-local f = require("custom.f")
-local fn = f.fn
 
 set("i", "jk", "<Esc>")
 set("v", "q", "<Esc>")
@@ -28,23 +26,16 @@ set("n", "<M-.>", "<c-w>5>")
 set("n", "<M-t>", "<C-W>+")
 set("n", "<M-s>", "<C-W>-")
 
--- copy current buffer absolute file path to system clipboard
-set("n", "<leader>YY", function()
-	vim.fn.setreg("+", vim.fn.expand("%:p"))
-	vim.notify("Copied file path to clipboard")
-end)
-
 -- quickfix, loclist nav
 set("n", "]]", "<cmd>cnext<CR>")
 set("n", "[[", "<cmd>cprev<CR>")
 
 -- lsp
+set("n", "K", vim.lsp.buf.hover)
+set("n", "grr", vim.lsp.buf.references)
 set("n", "gd", vim.lsp.buf.definition)
 set("n", "gD", vim.lsp.buf.declaration)
-set("n", "gT", vim.lsp.buf.type_definition)
 set("n", "<leader>ca", vim.lsp.buf.code_action)
-set("n", "]d", fn(vim.diagnostic.jump, { count = 1, float = true }))
-set("n", "[d", fn(vim.diagnostic.jump, { count = -1, float = true }))
 
 -- tabs
 set("n", "<left>", "gT")
--- nvim/.config/nvim/lua/lsp/on-attach.lua
+++ nvim/.config/nvim/lua/lsp/on-attach.lua
@@ -1,48 +1,36 @@
 local M = {}
 
 function M.setup()
-  local group = vim.api.nvim_create_augroup("user-lsp-attach", { clear = true })
+	local group = vim.api.nvim_create_augroup("user-lsp-attach", { clear = true })
 
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = group,
-    callback = function(args)
-      local bufnr = args.buf
-      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = group,
+		callback = function(args)
+			local bufnr = args.buf
+			local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
-			-- don't check markdown
-    	-- if vim.bo[bufnr].filetype == "markdown" or vim.bo[bufnr].filetype =="json" then
-    	--   vim.lsp.stop_client(client.id)
-    	-- end
+			local set = vim.keymap.set
 
-      local map = function(mode, lhs, rhs, desc)
-        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-      end
+			local ok_telescope, builtin = pcall(require, "telescope.builtin")
 
-      local ok_telescope, builtin = pcall(require, "telescope.builtin")
-
-    	map("n", "K", vim.lsp.buf.hover, "Hover")
-      map("n", "gD", vim.lsp.buf.declaration, "Declaration")
-      map("n", "gT", vim.lsp.buf.type_definition, "Type Definition")
-      map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
-
-      if ok_telescope then
-        map("n", "<leader>wd", builtin.lsp_document_symbols, "Document Symbols")
-        map("n", "<leader>ww", function()
-          builtin.diagnostics({ root_dir = true })
-        end, "Workspace Diagnostics")
+			if ok_telescope then
+				set("n", "<leader>wd", builtin.lsp_document_symbols)
+				set("n", "<leader>ww", function()
+					builtin.diagnostics({ root_dir = true })
+				end)
 			end
 
-      -- Example: disable semantic tokens for selected filetypes
-      local disable_semantic_tokens = {
-        -- lua = true,
-      }
+			-- Example: disable semantic tokens for selected filetypes
+			local disable_semantic_tokens = {
+				-- lua = true,
+			}
 
-      local ft = vim.bo[bufnr].filetype
-      if disable_semantic_tokens[ft] then
-        client.server_capabilities.semanticTokensProvider = nil
-      end
-    end,
-  })
+			local ft = vim.bo[bufnr].filetype
+			if disable_semantic_tokens[ft] then
+				client.server_capabilities.semanticTokensProvider = nil
+			end
+		end,
+	})
 end
 
 return M
