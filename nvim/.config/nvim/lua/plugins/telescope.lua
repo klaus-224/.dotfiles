de5647e40f3b1c79be06313e567f1850908db904
klaus-224 <rohinesh.ram@protonmail.com>
2026-04-21 10:29:16 -0600

    nvim/random

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -86,14 +86,12 @@ return {
 			-- KEYMAPS
 			local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader><leader>",
-				function()
-					builtin.find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
-				end
-			)
+			vim.keymap.set("n", "<leader><leader>", function()
+				builtin.find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
+			end)
 			vim.keymap.set("n", "?", builtin.current_buffer_fuzzy_find)
-			vim.keymap.set("n", "<leader>FS", builtin.lsp_workspace_symbols, {})
-			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
+			vim.keymap.set("n", "<leader>FS", builtin.lsp_workspace_symbols)
+			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols)
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags)
 			vim.keymap.set("n", "<leader>ff", builtin.buffers)
 			vim.keymap.set("n", "<leader>li", builtin.lsp_incoming_calls)
