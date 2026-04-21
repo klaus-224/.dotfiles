39ccab13b88e27318605fbb41371607153dd8386
klaus <klaus224@klauss-MacBook-Pro.local>
2026-04-20 22:29:10 -0600

    nvim/telescope

--- nvim/.config/nvim/lua/after/lsp/basedpyright_ls.lua
+++ nvim/.config/nvim/lua/after/lsp/basedpyright_ls.lua
@@ -1,25 +1,21 @@
 local client_config = require("lsp.client-config")
 
 return vim.tbl_deep_extend("force", client_config.base(), {
-  cmd = { "basedpyright-langserver", "--stdio" },
-  filetypes = { "python" },
-  root_markers = {
-    "pyproject.toml",
-    "pyrightconfig.json",
-    "ruff.toml",
-    ".ruff.toml",
-    ".git",
-  },
-  settings = {
-    basedpyright = {
-      analysis = {
-        typeCheckingMode = "standard",
-        diagnosticMode = "openFilesOnly",
-        useLibraryCodeForTypes = true,
-      },
-    },
-    pyright = {
-      disableOrganizeImports = true,
-    },
-  },
+	cmd = { "basedpyright-langserver", "--stdio" },
+	filetypes = { "python" },
+	root_markers = {
+		".git",
+	},
+	settings = {
+		basedpyright = {
+			analysis = {
+				typeCheckingMode = "standard",
+				diagnosticMode = "openFilesOnly",
+				useLibraryCodeForTypes = true,
+			},
+		},
+		pyright = {
+			disableOrganizeImports = true,
+		},
+	},
 })
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -80,65 +80,26 @@ return {
 							}
 						end,
 					},
-					git_branches = {
-						mappings = {
-							n = {
-								["<CR>"] = function(prompt_bufnr)
-									local action_state = require("telescope.actions.state")
-									local entry = action_state.get_selected_entry()
-
-									if #entry < 1 or entry[1] == nil then
-										vim.notify("No Diff")
-										return
-									end
-
-									telescope_actions.close(prompt_bufnr)
-
-									print(entry[1])
-
-									local file = entry.path
-
-									vim.cmd("vsplit")
-									vim.cmd("read !git show HEAD~1:" .. file)
-									vim.cmd("0d_") -- remove empty first line
-									vim.cmd("diffthis")
-									vim.cmd("wincmd p")
-									vim.cmd("diffthis")
-								end,
-							},
-						},
-					},
 				},
 			})
 
 			-- KEYMAPS
 			local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader><leader>", function()
-				builtin.find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
-			end, { desc = "Telescope find files" })
-			vim.keymap.set("n", "?", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy search in current buffer" })
+			vim.keymap.set("n", "<leader><leader>",
+				function()
+					builtin.find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
+				end
+			)
+			vim.keymap.set("n", "?", builtin.current_buffer_fuzzy_find)
 			vim.keymap.set("n", "<leader>FS", builtin.lsp_workspace_symbols, {})
 			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
-			vim.keymap.set("n", "<leader>fd", builtin.diagnostics)
-			vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
-
-			vim.keymap.set("n", "<leader>fb", builtin.git_branches, { desc = "List git branches" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+			vim.keymap.set("n", "<leader>ff", builtin.buffers)
+			vim.keymap.set("n", "<leader>li", builtin.lsp_incoming_calls)
+			vim.keymap.set("n", "<leader>lo", builtin.lsp_outgoing_calls)
 
 			require("custom.multigrep").setup()
 		end,
 	},
-	-- {
-	-- 	"nvim-telescope/telescope-ui-select.nvim",
-	-- 	config = function()
-	-- 		require("telescope").setup({
-	-- 			extensions = {
-	-- 				["ui-select"] = {
-	-- 					require("telescope.themes").get_dropdown({}),
-	-- 				},
-	-- 			},
-	-- 		})
-	-- 		require("telescope").load_extension("ui-select")
-	-- 	end,
-	-- },
 }
