03ee7c1d6b81a4a5863f24112a3aea198f5feeac
klaus-224 <rohinesh.ram@protonmail.com>
2026-04-23 11:14:02 -0600

    git/gh-dash+delta

--- nvim/.config/nvim/lua/after/lsp/yaml_ls.lua
+++ nvim/.config/nvim/lua/after/lsp/yaml_ls.lua
@@ -2,19 +2,21 @@ local client_config = require("lsp.client-config")
 
 return vim.tbl_deep_extend("force", client_config.base(), {
 	cmd = { "yaml-language-server", "--stdio" },
-	filetypes = { "yaml", "yaml.docker-compose", "yaml.ansible", "yaml.gitlab", "yaml.helm-values" },
-	root_markers = { ".git" },
+	filetypes = { "yaml", "yml" },
 	settings = {
-		schemaStore = {
-			-- You must disable built-in schemaStore support if you want to use
-			-- this plugin and its advanced options like `ignore`.
-			enable = false,
-			-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-			url = "",
+		yaml = {
+			schemaStore = {
+				-- You must disable built-in schemaStore support if you want to use
+				-- this plugin and its advanced options like `ignore`.
+				enable = false,
+				-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+				url = "",
+			},
+			schemas = require("schemastore").yaml.schemas(),
 		},
-		schemas = require('schemastore').yaml.schemas(),
 	},
 	on_init = function(client)
 		client.server_capabilities.documentFormattingProvider = true
 	end,
+	root_markers = { ".git" },
 })
