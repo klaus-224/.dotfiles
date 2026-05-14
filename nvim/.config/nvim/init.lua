89859cb3a8544d0db501a35572e8cdeaf8fa9226
klaus-224 <rohinesh.ram@protonmail.com>
2026-05-14 13:55:56 -0600

    nvim/sql

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -106,6 +106,7 @@ require('conform').setup({
     jsonc = { 'biome', 'prettier' },
     yaml = { 'prettier' },
     markdown = { 'prettier' },
+    sql = { 'sleek' },
 
     -- python
     python = { 'ruff_format' },
@@ -248,28 +249,6 @@ require('fzf-lua').setup({
 
 -- highlights
 require('nvim-highlight-colors').setup({})
-
--- load dadbod
-vim.api.nvim_create_user_command('DBUIT', function()
-  vim.g.db_ui_use_nerd_fonts = 1
-  vim.g.db_ui_show_database_icon = 1
-  vim.g.db_ui_winwidth = 30
-  vim.g.db_ui_disable_info_notifications = 1
-  vim.g.dbs = {}
-  vim.g.db_ui_execute_on_save = 0
-  vim.g.db_ui_table_helpers = {
-    duckdb = {
-      List = 'SELECT * FROM {table} LIMIT 200',
-      Count = 'SELECT COUNT(*) AS count FROM {table}',
-      Describe = 'DESCRIBE {table}',
-      Summarize = 'SUMMARIZE {table}',
-      Explain = 'EXPLAIN {last_query}',
-      Sample = 'SELECT * FROM {table} USING SAMPLE 25 ROWS',
-    },
-  }
-  vim.cmd('tabnew')
-  vim.cmd('DBUI')
-end, {})
 
 -- gitblame
 require('gitblame').setup({
