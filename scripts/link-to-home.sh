d9bd7ae41508f3c2e6b3b1a832a3bd09ef2a9a38
klaus-224 <rohinesh.ram@protonmail.com>
2026-08-24 13:54:22 -0600

    git thing onces more...

--- scripts/link-to-home.sh
+++ scripts/link-to-home.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 DOTFILES_HOME="${DOTFILES_HOME:-"$HOME/.dotfiles/"}"
 
-ln "$DOTFILES_HOME/ripgrep/.ripgreprc" "$HOME/.ripgreprc" 
+ln "$DOTFILES_HOME/ripgrep/.ripgreprc" "$HOME/.ripgreprc"
+ln -sf "$DOTFILES_HOME/lazygit/config.yml" "$HOME/Library/Application\ Support/lazygit/config.yml"
