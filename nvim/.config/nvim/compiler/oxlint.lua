822565f9794b21cc8130cc86538bcab4f8ad76fc
klaus <klaus224@klaus-macbook.local>
2026-09-01 00:03:14 -0600

    yeah

--- nvim/.config/nvim/compiler/oxlint.lua
+++ nvim/.config/nvim/compiler/oxlint.lua
@@ -4,11 +4,11 @@ if vim.b.current_compiler then
   return
 end
 
-vim.b.current_compiler = "oxlint"
+vim.b.current_compiler = 'oxlint'
 
-vim.opt_local.makeprg = "pnpm oxlint --format=unix"
+vim.opt_local.makeprg = 'oxlint . --format=unix'
 
 vim.opt_local.errorformat = {
-  "%f:%l:%c: %m [%t%*[^/]/%*[^]]]",
-  "%-G%.%#",
+  '%f:%l:%c: %m [%t%*[^/]/%*[^]]]',
+  '%-G%.%#',
 }
