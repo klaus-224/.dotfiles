114e2cb070915ca9f3ce10f0b8c09950b5d556f7
klaus-224 <rohinesh.ram@protonmail.com>
2026-09-08 10:49:49 -0600

    repair lock

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -38,21 +38,8 @@ require('lazydev').setup({
 
 vim.lsp.enable({
   'lua_ls',
-  -- 'basedpyright',
-  -- 'bashls',
-  -- 'cssls',
-  -- 'dockerls',
-  -- 'html_ls',
-  -- 'jsonls',
-  -- 'prismals',
-  -- 'ruff',
-  -- 'rust_analyzer',
-  -- 'sqlls',
   'postgres_lsp',
-  -- 'zshcs',
-  -- 'svelte',
   'tailwindcss',
-  -- 'terraformls',
   'ts_go',
   'yamlls',
   'tombi',
@@ -118,10 +105,10 @@ cmp.setup({
 })
 
 require('tree-sitter-manager').setup({
-  dependencies = {},   -- tree-sitter CLI must be installed system-wide
+  dependencies = {}, 
   ensure_installed = { 'svelte' },
-  auto_install = true, -- install missing parsers when editing a new file
-  highlight = false,   -- treesitter highlighting is enabled by default
+  auto_install = true,
+  highlight = false,
 })
 
 require('oil').setup({
@@ -149,6 +136,6 @@ require('gitblame').setup({
   enabled = false,
   message_template = ' <summary> • <date> • <author> • <<sha>>',
   date_format = '%r',
-  virtual_text_column = 0,
+  virtual_text_column = 80,
   use_blame_commit_file_urls = false,
 })
