ff730251bef94c8c106acc22aa8c9291b3a60822
klaus <klaus224@klaus-macbook.local>
2026-09-10 12:12:42 -0600

    chore(nix): establish migration baseline

--- nix/home.nix
+++ nix/home.nix
@@ -5,9 +5,47 @@
   home.homeDirectory = "/Users/klaus224";
   home.stateVersion = "26.05";
   home.packages = with pkgs; [
-
+    aws-cdk
+    awscli2
+    bat
+    bottom
+    duckdb
+    fd
+    gcc
+    gh
+    glow
+    jq
+    just
+    lazygit
+    lua
+    lua-language-server
+    neovim
+    ripgrep
+    rustup
+    sqlite
+    supabase-cli
+    terraform
+    tmux
+    tree
+    tree-sitter
+    uv
+    watchman
+    yaml-language-server
   ];
 
-  xdg.configFile."nvim".source = ../nvim;
-  xdg.configFile."ghostty".source = ../ghostty;
-}
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+  
+  programs.fzf.enable = true;
+  programs.starship.enable = true;
+  programs.git.enable = true;
+  programs.eza.enable = true;
+
+  xdg.configFile."nvim".source =
+    config.lib.file.mkOutOfStoreSymlink "/Users/klaus224/.dotfiles/nvim";
+
+  xdg.configFile."ghostty".source =
+    config.lib.file.mkOutOfStoreSymlink "/Users/klaus224/.dotfiles/ghostty";}
