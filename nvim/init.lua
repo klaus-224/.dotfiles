2c061ff59d8994f1be7f11dc721bb678381d25ac
klaus224 <rohinesh.ram@protonmail.com>
2026-09-12 23:15:21 -0600

    feat(devenv): dotfiles devenv

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,9 +21,7 @@ vim.pack.add({
   { src = 'https://github.com/tpope/vim-dadbod' },
   { src = 'https://github.com/kristijanhusak/vim-dadbod-ui' },
   { src = 'https://github.com/kristijanhusak/vim-dadbod-completion' },
-  {
-    src = "https://github.com/nvim-treesitter/nvim-treesitter",
-  },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = 'https://github.com/f-person/git-blame.nvim' },
 })
 
