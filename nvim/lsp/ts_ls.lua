fa865710049d15dc89af519e57a05199fa5d3352
klaus224 <rohinesh.ram@protonmail.com>
2026-09-12 21:34:16 -0600

    feat(nvim): add ts, sql, postgres support

---
+++ nvim/lsp/ts_ls.lua
@@ -0,0 +1,174 @@
+---@brief
+---
+--- https://github.com/typescript-language-server/typescript-language-server
+---
+--- `ts_ls`, aka `typescript-language-server`, is a Language Server Protocol implementation for TypeScript wrapping `tsserver`. Note that `ts_ls` is not `tsserver`.
+---
+--- While `typescript-language-server` will likely eventually be replaced by [tsc](#tsc), the official language server
+--- for TypeScript, `typescript-language-server` may still be useful for features such as es5 support.
+---
+--- `typescript-language-server` depends on `typescript`. Both packages can be installed via `npm`:
+--- ```sh
+--- # if you are using `ts_ls` you will need to install a `typescript` version < 7 for now
+--- npm install -g typescript@<version> typescript-language-server
+--- ```
+---
+--- To configure typescript language server, add a
+--- [`tsconfig.json`](https://www.typescriptlang.org/docs/handbook/tsconfig-json.html) or
+--- [`jsconfig.json`](https://code.visualstudio.com/docs/languages/jsconfig) to the root of your
+--- project.
+---
+--- Here's an example that disables type checking in JavaScript files.
+---
+--- ```json
+--- {
+---   "compilerOptions": {
+---     "module": "commonjs",
+---     "target": "es6",
+---     "checkJs": false
+---   },
+---   "exclude": [
+---     "node_modules"
+---   ]
+--- }
+--- ```
+---
+--- If you also have a `deno.json` or a `deno.jsonc` in your project, the `ts_ls` may not start (depending where the
+--- deno.json is)
+---
+--- Use the `:LspTypescriptSourceAction` command to see "whole file" ("source") code-actions such as:
+--- - organize imports
+--- - remove unused code
+---
+--- Use the `:LspTypescriptGoToSourceDefinition` command to navigate to the source definition of a symbol (e.g., jump to the original implementation instead of type definitions).
+---
+--- ### Monorepo support
+---
+--- See the monorepo support documentation for [tsc](#tsc)
+
+---@type vim.lsp.Config
+return {
+  init_options = { hostInfo = 'neovim' },
+  cmd = function(dispatchers, config)
+    local cmd = 'typescript-language-server'
+    if (config or {}).root_dir then
+      local local_cmd = vim.fs.joinpath(config.root_dir, 'node_modules/.bin', cmd)
+      if vim.fn.executable(local_cmd) == 1 then
+        cmd = local_cmd
+      end
+    end
+    return vim.lsp.rpc.start({ cmd, '--stdio' }, dispatchers)
+  end,
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+  },
+  oot_dir = function(bufnr, on_dir)
+    local root = vim.fs.root(bufnr, {
+      {
+        "pnpm-lock.yaml",
+        "package-lock.json",
+        "yarn.lock",
+        "bun.lock",
+        "bun.lockb",
+      },
+      ".git",
+    }) or vim.fn.getcwd()
+
+    local local_cmd = vim.fs.joinpath(
+      root,
+      "node_modules",
+      ".bin",
+      "typescript-language-server"
+    )
+
+    if vim.fn.executable(local_cmd) ~= 1
+        and vim.fn.executable("typescript-language-server") ~= 1
+    then
+      return
+    end
+
+    on_dir(root)
+  end,
+  handlers = {
+    -- handle rename request for certain code actions like extracting functions / types
+    ['_typescript.rename'] = function(_, result, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      vim.lsp.util.show_document({
+        uri = result.textDocument.uri,
+        range = {
+          start = result.position,
+          ['end'] = result.position,
+        },
+      }, client.offset_encoding)
+      vim.lsp.buf.rename()
+      return vim.NIL
+    end,
+  },
+  commands = {
+    ['editor.action.showReferences'] = function(command, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      local file_uri, position, references = unpack(command.arguments)
+
+      local quickfix_items = vim.lsp.util.locations_to_items(references --[[@as any]], client.offset_encoding)
+      vim.fn.setqflist({}, ' ', {
+        title = command.title,
+        items = quickfix_items,
+        context = {
+          command = command,
+          bufnr = ctx.bufnr,
+        },
+      })
+
+      vim.lsp.util.show_document({
+        uri = file_uri --[[@as string]],
+        range = {
+          start = position --[[@as lsp.Position]],
+          ['end'] = position --[[@as lsp.Position]],
+        },
+      }, client.offset_encoding)
+      ---@diagnostic enable: assign-type-mismatch
+
+      vim.cmd('botright copen')
+    end,
+  },
+  on_attach = function(client, bufnr)
+    -- ts_ls provides `source.*` code actions that apply to the whole file. These only appear in
+    -- `vim.lsp.buf.code_action()` if specified in `context.only`.
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspTypescriptSourceAction', function()
+      local source_actions = vim.tbl_filter(function(action)
+        return vim.startswith(action, 'source.')
+      end, client.server_capabilities.codeActionProvider.codeActionKinds)
+
+      vim.lsp.buf.code_action({
+        context = {
+          only = source_actions,
+          diagnostics = {},
+        },
+      })
+    end, {})
+
+    -- Go to source definition command
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspTypescriptGoToSourceDefinition', function()
+      local win = vim.api.nvim_get_current_win()
+      local params = vim.lsp.util.make_position_params(win, client.offset_encoding)
+      client:exec_cmd({
+        command = '_typescript.goToSourceDefinition',
+        title = 'Go to source definition',
+        arguments = { params.textDocument.uri, params.position },
+      }, { bufnr = bufnr }, function(err, result)
+        if err then
+          vim.notify('Go to source definition failed: ' .. err.message, vim.log.levels.ERROR)
+          return
+        end
+        if not result or vim.tbl_isempty(result) then
+          vim.notify('No source definition found', vim.log.levels.INFO)
+          return
+        end
+        vim.lsp.util.show_document(result[1], client.offset_encoding, { focus = true })
+      end)
+    end, { desc = 'Go to source definition' })
+  end,
+}
