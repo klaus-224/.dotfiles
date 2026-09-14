1d6e2fc3b5f5efce10ec9c707759fa32485dda38
klaus-224 <rohinesh.ram@protonmail.com>
2026-09-14 10:40:40 -0600

    fix(devenv): broken, install globally

--- nvim/lsp/sqlls.lua
+++ nvim/lsp/sqlls.lua
@@ -1,19 +1,19 @@
 ---@brief
 ---
---- https://github.com/joe-re/sql-language-server
+--- https://github.com/sqls-server/sqls
 ---
---- Install with `npm install -g sql-language-server`.
-
--- return if sql-language-server is not installed
-if vim.fn.executable("sql-language-server") ~= 1 then
-  return {}
-end
+--- ```lua
+--- vim.lsp.config('sqls', {
+---   cmd = {"path/to/command", "-config", "path/to/config.yml"};
+---   ...
+--- })
+--- ```
+--- Sqls can be installed via `go install github.com/sqls-server/sqls@latest`. Instructions for compiling Sqls from the source can be found at [sqls-server/sqls](https://github.com/sqls-server/sqls).
 
 ---@type vim.lsp.Config
 return {
-  cmd = { "sql-language-server", "up", "--method", "stdio" },
-  filetypes = { "sql", "mysql" },
-  root_dir = function(bufnr, on_dir)
-    on_dir(vim.fs.root(bufnr, { ".sqllsrc.json", ".git" }) or vim.fn.getcwd())
-  end,
+  cmd = { 'sqls' },
+  filetypes = { 'sql', 'mysql' },
+  root_markers = { 'config.yml' },
+  settings = {},
 }
